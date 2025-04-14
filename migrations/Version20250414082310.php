4bd8b9e7f4a75f073b20a6b1f525cc4f3b2892de
deadlord082 <135032460+deadlord082@users.noreply.github.com>
2025-04-14 09:51:26 +0200

    initial commit

---
+++ migrations/Version20250414082310.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250404082310 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE "article" (id SERIAL NOT NULL, title VARCHAR(255) NOT NULL, content VARCHAR(255) NOT NULL, user_id int(255) NOT NULL, date VARCHAR(255) NOT NULL, PRIMARY KEY(id))
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql(<<<'SQL'
+            DROP TABLE "article"
+        SQL);
+    }
+}
