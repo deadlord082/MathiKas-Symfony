05301f4a6d0fbd532208ff34289647dfcf27cb41
deadlord082 <135032460+deadlord082@users.noreply.github.com>
2025-04-28 12:17:46 +0200

    added article browse

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -25,6 +25,39 @@ final class ArticleController extends AbstractController
         ]);   
     }
 
+    #[Route("/add-article", name: "app_add_article")]
+    public function add(Request $request, EntityManagerInterface $em): Response
+    {
+        // Récupère le user depuis la session
+        $user = $this->security->getUser();
+    
+        //1. On prépare un objet vide (cible du form)
+        $article = new article();
+        $article->settitle('testTitre');
+        $article->setcontent('testContenu');
+        $article->setAuthor($user);
+        $article->setDate(new \DateTimeImmutable('tomorrow'));
+
+        //2. Crée le form via la formbuilder et la classe préparée
+        $form = $this->createForm(ArticleType::class, $article);
+
+        //Est-ce qu'on est en train de traiter le form ?
+        //Set le champ isSubmitted pour nous
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            //Traitement du formulaire
+            $article = $form->getData();
+            //La j'ai un objet correct, je peux le persister
+            $em->persist($article);
+            $em->flush();
+        }
+
+        //3.On passe le form à la vue
+        return $this->render('article/add.html.twig', [
+            'form' => $form
+        ]);
+    }
+
     #[Route('/edit-article/', name: 'app_edit_article')]
     public function new(Request $request, EntityManagerInterface $em): Response
     { 
@@ -49,7 +82,6 @@ final class ArticleController extends AbstractController
             $em->persist($article);
             $em->flush();
         }
-
 
         //3.On passe le form à la vue
         return $this->render('article/index.html.twig', [
